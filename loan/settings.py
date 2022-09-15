2f16cb3f832003e5759d398ae0304c3e8f319c09
Snex-Thadeus <lwande94@gmail.com>
2022-09-16 00:59:44 +0300

    Staticfiles

--- loan/settings.py
+++ loan/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-local")
+STATIC_ROOT = os.path.join(BASE_DIR, "static-local")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
